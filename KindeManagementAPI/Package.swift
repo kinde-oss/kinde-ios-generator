4bda4434ba4ca30fe6fd14ccae8b1c1689a439a5
Tanzilya <Yakshimbetova.tanzilya@gmail.com>
2023-04-10 12:17:14 +0200

    - update Auth class, do refactoring

--- KindeManagementAPI/Package.swift
+++ KindeManagementAPI/Package.swift
@@ -3,30 +3,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenAPIClient",
+    name: "KindeSDK",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_11),
-        .tvOS(.v9),
-        .watchOS(.v3),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OpenAPIClient",
-            targets: ["OpenAPIClient"]
+            name: "KindeSDK",
+            targets: ["KindeSDK"]
         ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+	.package(name: "AppAuth", url: "https://github.com/openid/AppAuth-iOS.git", from: "1.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OpenAPIClient",
-            dependencies: [],
-            path: "OpenAPIClient/Classes"
+            name: "KindeSDK",
+            dependencies: ["AppAuth", ],
+            path: "KindeSDK/"
         ),
     ]
 )
