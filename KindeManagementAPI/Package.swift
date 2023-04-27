ee8914595a13c6bade59d641839596cca726c522
Tanzilya <Yakshimbetova.tanzilya@gmail.com>
2023-04-27 19:52:55 +0200

    - update sdk

--- KindeManagementAPI/Package.swift
+++ KindeManagementAPI/Package.swift
@@ -16,14 +16,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-	.package(name: "AppAuth", url: "https://github.com/openid/AppAuth-iOS.git", from: "1.6.0")
+	.package(url: "https://github.com/openid/AppAuth-iOS.git", from: "1.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KindeSDK",
-            dependencies: ["AppAuth", ],
+            dependencies: [
+		.product(name: "AppAuth", package: "AppAuth-iOS")
+		 ],
             path: "KindeSDK/"
         ),
     ]
